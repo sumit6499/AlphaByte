3f8529dc2f69873c695c1bc6ef78ad407a5f584e
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-03-16 23:46:42 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,28 +4,28 @@ import Navbar from './components/Navbar';
 import Login from './components/Login';
 import AdminLogin from './components/AdminLogin';
 import CandidateLogin from './components/CandidateLogin';
-import AdminSignup from './components/AdminSignup'; // Import the AdminSignup component
-import CandidateSignup from './components/CandidateSignup'; // Import the CandidateSignup component
+import AdminSignup from './components/AdminSignup';
+import CandidateSignup from './components/CandidateSignup';
 import { BrowserRouter } from "react-router-dom";
-import Home from  './components/Home';
-import ResumeUploader from './components/ResumeUploader'; // Import the ResumeUploader component
+import Home from './components/Home';
+import ResumeUploader from './components/ResumeUploader';
+import ErrorPage from './components/ErrorPage'; // Import the ErrorPage component
 
 const App = () => {
   return (
-    
     <BrowserRouter>
       <div>
         <Navbar />
         <Routes>
-      
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/admin-login" element={<AdminLogin />} />
           <Route path="/candidate-login" element={<CandidateLogin />} />
-          <Route path="/admin-Signup" element={<AdminSignup />} /> {/* Route to the AdminSignup component */}
-          <Route path="/candidate-Signup" element={<CandidateSignup />} /> {/* Route to the CandidateSignup component */}
-          <Route path="/resume-uploader" element={<ResumeUploader />} /> {/* Add route for the ResumeUploader component */}
-
+          <Route path="/admin-signup" element={<AdminSignup />} />
+          <Route path="/candidate-signup" element={<CandidateSignup />} />
+          <Route path="/resume-uploader" element={<ResumeUploader />} />
+          {/* Catch-all route for any unmatched URLs */}
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </div>
     </BrowserRouter>
