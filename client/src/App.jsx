ab4b850e61b4153f9a0da8c6fc1217512c477c9f
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-03-16 22:10:43 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,30 @@
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import Services from "./components/Services";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Login from './components/Login';
+import AdminLogin from './components/AdminLogin';
+import CandidateLogin from './components/CandidateLogin';
+import AdminSignup from './components/AdminSignup'; // Import the AdminSignup component
+import CandidateSignup from './components/CandidateSignup'; // Import the CandidateSignup component
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <Services />
-    </>
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
+          <Route path="/candidate-login" element={<CandidateLogin />} />
+          <Route path="/admin-Signup" element={<AdminSignup />} /> {/* Route to the AdminSignup component */}
+          <Route path="/candidate-Signup" element={<CandidateSignup />} /> {/* Route to the CandidateSignup component */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
