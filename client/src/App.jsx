30bb33c64c09370ec80bc58324db47cc8ee79dc1
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-03-17 00:57:29 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,39 @@
+
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
 import AdminLogin from './components/AdminLogin';
 import CandidateLogin from './components/CandidateLogin';
-import AdminSignup from './components/AdminSignup';
-import CandidateSignup from './components/CandidateSignup';
+import AdminSignup from './components/AdminSignup'; // Import the AdminSignup component
+import CandidateSignup from './components/CandidateSignup'; // Import the CandidateSignup component
 import { BrowserRouter } from "react-router-dom";
-import Home from './components/Home';
-import ResumeUploader from './components/ResumeUploader';
+import Home from  './components/Home';
+import ResumeUploader from './components/ResumeUploader'; // Import the ResumeUploader component
+import JobListing from './components/JobListing';
 import ErrorPage from './components/ErrorPage'; // Import the ErrorPage component
+
 
 const App = () => {
   return (
+    
     <BrowserRouter>
       <div>
         <Navbar />
         <Routes>
+      
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/admin-login" element={<AdminLogin />} />
           <Route path="/candidate-login" element={<CandidateLogin />} />
-          <Route path="/admin-signup" element={<AdminSignup />} />
-          <Route path="/candidate-signup" element={<CandidateSignup />} />
-          <Route path="/resume-uploader" element={<ResumeUploader />} />
-          {/* Catch-all route for any unmatched URLs */}
+          <Route path="/admin-Signup" element={<AdminSignup />} /> {/* Route to the AdminSignup component */}
+          <Route path="/candidate-Signup" element={<CandidateSignup />} /> {/* Route to the CandidateSignup component */}
+          <Route path="/resume-uploader" element={<ResumeUploader />} /> {/* Add route for the ResumeUploader component */}
+          <Route path="/Job-listing" element={<JobListing/>} />
+
+
           <Route path="*" element={<ErrorPage />} />
+
         </Routes>
       </div>
     </BrowserRouter>
