ff63428451556ec576214478b4f84c4b0007150a
Zeeshan <zishuz44@gmail.com>
2024-03-17 07:56:09 +0530

    update footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,4 @@
-
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
@@ -11,16 +10,27 @@ const Footer = () => {
             <h2 className="text-lg font-semibold mb-4">Contact Us</h2>
             <p>Email: placemint@gmail.com</p>
             <p>Phone: +1234567890</p>
-            <p>Address: Rajwada, P.O.Box. No.130, Ichalkaranji-416115, Kolhapur. </p>
+            <p>
+              Address: Ace Complex, P.O.Box. No.130, Ichalkaranji-416115,
+              Kolhapur.{" "}
+            </p>
           </div>
           {/* Quick Links Section */}
           <div>
             <h2 className="text-lg font-semibold mb-4">Quick Links</h2>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/services">Services</a></li>
-              <li><a href="/contact">Contact</a></li>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/services">Services</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
             </ul>
           </div>
           {/* Social Media Section */}
@@ -28,10 +38,9 @@ const Footer = () => {
             <h2 className="text-lg font-semibold mb-4">Follow Us</h2>
             <div className="flex space-x-4">
               <a href="#" className="text-white hover:text-gray-400">
-                
                 <i className="fab fa-facebook-f">Instagram</i>
               </a>
-              <br/>
+              <br />
               <a href="#" className="text-white hover:text-gray-400">
                 <i className="fab fa-twitter">Facebook</i>
               </a>
